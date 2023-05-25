288a79f61986ca148fa2a2b37d84a2c9023c400f
dimakuchinskyi <kuci.ia77@gmail.com>
2023-05-25 20:50:50 +0300

    Бонус

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-import requests
+import  requests
 from bs4 import BeautifulSoup
-response = requests.get('https://www.example.com/')
+response = requests.get('https://weather.com/uk-UA/weather/'
+                        'today/l/2261b41a35498e667410cb84dfb'
+                        '95d455f977b64d2c02b673df410b7d9ec31ed')
 if response.status_code == 200:
-    soup = BeautifulSoup(response.text, features='html.parser')
-    for script in soup.find_all(['style', 'script']):
-        script.extract()#Вирізає HTML приколи
-        text = ' '.join(soup.stripped_strings)#Видрізає зайве HTML приколи
-        words = len(text.split())
-        print(words)
+    soup = BeautifulSoup(response.text, features= "html.parser")
+    city = soup.find('h1',{"class":"CurrentConditions--location--1YWj_"})
+    weather = soup.find('span')
+    if city:
+        city_name = city.text.strip()
+        weather = weather.text.strip()
+        print(city_name)
+        print(f'Температура - {weather}')
+    else:
+        print('Місто не знайдено ')
+except:
+    print('Немає підключння')
