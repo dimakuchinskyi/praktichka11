cbc5f01c4ba5acd6cd54f79996411acdaae2b96b
dimakuchinskyi <kuci.ia77@gmail.com>
2023-05-25 20:29:54 +0300

    #4

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ from bs4 import BeautifulSoup
 response = requests.get('https://www.example.com/')
 if response.status_code == 200:
     soup = BeautifulSoup(response.text, features='html.parser')
-    soup_list = soup.find_all('a')
-    for link in soup_list:
-        href = link.get('href')
-        print(href)
-        if href.startswith('https://'):
-            print(href)
+    for script in soup.find_all(['style', 'script']):
+        script.extract()#Вирізає HTML приколи
+        text = ' '.join(soup.stripped_strings)#Видрізає зайве HTML приколи
+        words = len(text.split())
+        print(words)
