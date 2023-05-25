2ceb5065a02de010268aa2f5c0f46cb572686127
dimakuchinskyi <kuci.ia77@gmail.com>
2023-05-25 20:00:45 +0300

    #2

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
-response = requests.get('https://www.example.com/')
+response = requests.get('https://uk.wikipedia.org/')
 
 if response.status_code == 200:
     soup = BeautifulSoup(response.content, 'html.parser')
-    title = soup.find('title').text
-    print(title)
-else:
-    print('Немає підключення ', response.status_code)
+    img = soup.find_all('img')#Список
+    for i in img:
+        print('https://' + i['src'])
