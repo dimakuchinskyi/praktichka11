a4b4d51341a757dfc24c326199b732eb2ff7d893
dimakuchinskyi <kuci.ia77@gmail.com>
2023-05-25 19:51:54 +0300

    #1

---
+++ main.py
@@ -0,0 +1,10 @@
+import requests
+from bs4 import BeautifulSoup
+response = requests.get('https://www.example.com/')
+
+if response.status_code == 200:
+    soup = BeautifulSoup(response.content, 'html.parser')
+    title = soup.find('title').text
+    print(title)
+else:
+    print('Немає підключення ', response.status_code)
